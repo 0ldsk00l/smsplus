fb1105faa3a15efbbee6a9720b61a991ff19a0ef
rdanbrook <1869094+rdanbrook@users.noreply.github.com>
2018-02-24 18:11:10 -0500

    Prevent window resizing

--- shell/smsplus.c
+++ shell/smsplus.c
@@ -359,6 +359,7 @@ int main (int argc, char *argv[]) {
 	
 	// Set the GL version
 	glfwDefaultWindowHints();
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
